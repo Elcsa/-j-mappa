7e2970d3364eb5d44b05bb555d6c50c78914d712
Elcsa <81393593+Elcsa@users.noreply.github.com>
2022-11-10 15:54:45 +0100

    6het utani elso doga

---
+++ src/artwor_interface.ts
@@ -0,0 +1,5 @@
+export interface artwork{
+    title:string;
+    year:Date;
+    price:number;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,21 @@
-console.log("asd")
-let number=5;
+
+import{artwork}from './artwor_interface';
+
+
+let cim=(document.getElementById('cim')as HTMLInputElement)
+let year=(document.getElementById('year')as HTMLInputElement)
+let ar=(document.getElementById('ar')as HTMLInputElement)
+
+function cimcheck(cim:string):boolean{
+    if (cim.length>= 1 && cim !=""){
+    return true;
+    } return false;
+}
+function yearcheck(year:Date):boolean{
+    if(year.getFullYear > )return true;
+    return false;
+}
+function archeck(ar:number):boolean{
+    if(ar>=0){return true}
+    return false;
+}
